2ae1f21e91ea571b26c04583217d9acbaf6007e8
Roland Ukor <roland@firstlinq.com>
2015-02-05 15:44:28 +0000

    Change OM dependency and CLJS version

--- src/leiningen/new/chestnut/project.clj
+++ src/leiningen/new/chestnut/project.clj
@@ -9,12 +9,12 @@
   :test-paths ["spec/clj"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2511" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2760" :scope "provided"]
                  [ring "1.3.2"]
                  [ring/ring-defaults "0.1.3"]
                  [compojure "1.3.1"]
                  [enlive "1.1.5"]
-                 [om "0.8.0-rc1"]
+                 [org.omcljs/om "0.8.8"]
                  [environ "1.0.0"]{{{project-clj-deps}}}]
 
   :plugins [[lein-cljsbuild "1.0.3"]
@@ -54,7 +54,7 @@
                    :dependencies [[figwheel "0.2.1-SNAPSHOT"]
                                   [figwheel-sidecar "0.2.1-SNAPSHOT"]
                                   [com.cemerick/piggieback "0.1.3"]
-                                  [weasel "0.4.2"]{{{project-dev-deps}}}]
+                                  [weasel "0.5.0"]{{{project-dev-deps}}}]
 
                    :repl-options {:init-ns {{project-ns}}.server
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl{{{nrepl-middleware}}}]}
